9c3815f9f4f439a46f94d842aa60ac5b486c0e6b
Allen <dallen@middlebury.edu>
2020-09-11 12:00:06 -0400

    First push

---
+++ IxPopDyMod.R
@@ -0,0 +1,8 @@
+## Ixodes population dynamics model
+## Dave Allen and Myles Stokowski
+
+# read inputs
+host_community <- read.csv('inputs/host_community.csv')
+weather <- read.csv('inputs/weather.csv')
+tick_parameters <- read.csv('tic_parameters.csv')
+
