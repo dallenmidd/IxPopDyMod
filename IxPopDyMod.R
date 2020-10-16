2caef9dc703512cf4664707c44d65f8134d310cd
myles <mylesstokowski@gmail.com>
2020-10-16 16:28:19 -0400

    renamed transitions, added rest of development delays and oviposition delay

--- IxPopDyMod.R
+++ IxPopDyMod.R
@@ -20,7 +20,6 @@ a_pref <- c(0, 0, 1)
 l_feed_success <- c(0.49, 0.17, 0.49)
 host_rc <- c(0.92, 0.147, 0.046)
 
-
 # initialize a delay matrix with a row for each of the tick life_stages and
 # a column for each day we have weather data
 max_delay = 300
@@ -29,9 +28,8 @@ rownames(delay_mat) <- life_stages
 
 # also initialize a population matrix N
 N <- matrix(nrow = length(life_stages), ncol = dim(weather)[1], data = 0)
+N[,1] <- runif(length(life_stages), min = 1000, max = 1000)
 rownames(N) <- life_stages
-initial_pop <- runif(length(life_stages), min = 1000, max = 1000) %>% as.integer()
-N[,1] <- initial_pop
 
 # 01 functions to grab the parameters that determine the transition matrix at a given time
 get_temp <- function(time) {
@@ -41,26 +39,10 @@ get_temp <- function(time) {
 }
 
 get_vpd <- function(time) {
-  # DA comment: yeah all the other papers use RH, but I think the VPD is the more
-  # biologically relavent value, but if all the parameterization is iwth RH, we might
-  # have to go back to RH. Eitherway if you know the temp you can convert between RH and VPD
   weather %>% 
     filter(j_day %in% time) %>%
     pull(vpdmean)
 } 
-
-# The weather parameters for generating the transition matrix are single vals (return values of get_temp, get_vpd).
-# What host community data do we need and how will host densities be stored (format/data structure)?  
-# Currently, these functions return vectors of length n_host_spp
-# Parameters from research_strat p.4: 
-#   For the ith host species and jth tick life stage we have parameters defining the feeding preference, 
-#   pj,i, tick feeding success rate, sj,i , and reservoir competence, ri
-# Some of these parameters are based on tick and host sp., (pj,i and sj,i), does that mean that the host
-# community input (csv) would have some tick data as well? Then the tick_parameters input csv could 
-# just have parameters about weather impacts on transition probability
-get_host_densities <- function(time, host_community) {return(runif(n_host_spp))} 
-get_host_preferences <- function(tick_parameters) {return(runif(n_host_spp))}
-get_host_reservoir_competences <- function(host_community) {return(runif(n_host_spp))} # TODO based on tick params or host community?
 
 # 02 functional forms for transition probabilities
 
@@ -105,58 +87,44 @@ gen_trans_matrix <- function(time) {
   # get the parameters 
   temp = get_temp(time)
   vpd = get_vpd(time)
-  host_densities = get_host_densities(time)
-  
+
   # initialize the transition matrix with zeros
   n_life_stages <- length(life_stages)
   trans_matrix <- matrix(0, ncol = n_life_stages, nrow = n_life_stages, 
                          dimnames = list(life_stages, life_stages))
   
   # calculate the transition probabilities for possible transitions
-  trans_matrix['e', 'hl'] <- get_transition_fun("egg_larva", pred1 = temp)
-  trans_matrix['e', 'e'] <- 1 - trans_matrix['e', 'hl'] - get_transition_fun('egg_mort')
   
-  trans_matrix['hl', 'ql'] <- get_transition_fun('larva_harden')
-  trans_matrix['hl', 'hl'] <- 1 - trans_matrix['hl', 'ql'] - get_transition_fun('larva_mort')
+  trans_matrix['hl', 'ql'] <- get_transition_fun('hl_ql')
+  trans_matrix['hl', 'hl'] <- 1 - trans_matrix['hl', 'ql'] - get_transition_fun('l_m')
   
-   trans_matrix['ql', 'fl'] <- get_transition_fun('larva_quest', pred1 = temp) * get_transition_fun('host_finding', pred1 = sum(host_den * l_pref))
-   trans_matrix['ql', 'ql'] <- 1 - trans_matrix['ql', 'fl'] - get_transition_fun('larva_mort')
+  trans_matrix['ql', 'fl'] <- get_transition_fun('ql_fl', pred1 = temp) * get_transition_fun('q_f', pred1 = sum(host_den * l_pref))
+  trans_matrix['ql', 'ql'] <- 1 - trans_matrix['ql', 'fl'] - get_transition_fun('l_m')
   
-  # haven't actually handled infected/uninfected here (depends on hosts), this just says that there's 
-  # a 50/50 chance that an uninfected feeding larva or nymph is infected/uninfected when it becomes engorged
   # density dependent feeding success? Yikes, will need to track how many of each life stage on each host every day?????
   # for now ignore density dependent feeding success
   # this doesn't have time delay
+  # Myles note: not sure how to implement time delay here. What's different here is that one class goes to multiple, 
+  # and the transition probability is not only describing the chance of advancing to next stage, but also the 
+  # relative number of feeding ticks that become either infected or uninfected.
+  # Idea: maybe we have to split up the processes of becoming infected (interpret this is a probability) and 
+  # becoming engorged (interpret this as a time delay)
   trans_matrix['fl', 'eul'] <- sum((1-host_rc) * (l_feed_success * ( (host_den * l_pref)/sum(host_den * l_pref))))
   trans_matrix['fl', 'eil'] <- sum(host_rc* (l_feed_success * ( (host_den * l_pref)/sum(host_den * l_pref))))
-  trans_matrix['fl', 'fl'] <- 1 - trans_matrix['fl', 'eul'] - trans_matrix['fl', 'eil'] - get_transition_fun('larva_engorged_mort')
-  
-  # commented out engorged larvae to questing nymph: if we don't change the (zero) transition 
-  # probabilities for these stages, multiplication gives the desired behavior of  any engorged larvae 
-  # dropping out of the population matrix at the next time step (first we copy the # individuals to the delay matrix)
-  # trans_matrix['eul', 'qun'] <- get_transition_fun('larva_engorged_nymph', pred1 = temp)
-  # trans_matrix['eul', 'eul'] <- 1 - trans_matrix['eul', 'qun'] - get_transition_fun('larva_engorged_mort')
-  # trans_matrix['eil', 'qin'] <- get_transition_fun('larva_engorged_nymph', pred1 = temp)
-  # trans_matrix['eil', 'eil'] <- 1 - trans_matrix['eil', 'qin'] - get_transition_fun('larva_engorged_mort')
+  trans_matrix['fl', 'fl'] <- 1 - trans_matrix['fl', 'eul'] - trans_matrix['fl', 'eil'] - get_transition_fun('el_m')
   
   # trans_matrix['qun', 'fun'] skipped because depends on host community
   # trans_matrix['qun', 'qun'] ...
   # trans_matrix['qin', 'fin'] ...
   # trans_matrix['qin', 'qin'] ...
   
-  trans_matrix['fun', 'eun'] <- get_transition_fun('nymph_feed_engorged') / 2 # placeholder, dependent on hosts 
-  trans_matrix['fun', 'ein'] <- get_transition_fun('nymph_feed_engorged') / 2
-  trans_matrix['fun', 'fun'] <- 1 - trans_matrix['fun', 'eun'] - trans_matrix['fun', 'ein'] - get_transition_fun('nymph_engorged_mort')
+  trans_matrix['fun', 'eun'] <- get_transition_fun('fn_en') / 2 # placeholder, dependent on hosts 
+  trans_matrix['fun', 'ein'] <- get_transition_fun('fn_en') / 2
+  trans_matrix['fun', 'fun'] <- 1 - trans_matrix['fun', 'eun'] - trans_matrix['fun', 'ein'] - get_transition_fun('en_m')
   
-  trans_matrix['fin', 'ein'] <- get_transition_fun('nymph_feed_engorged')
-  trans_matrix['fin', 'fin'] <- 1 - trans_matrix['fin', 'ein'] - get_transition_fun('nymph_engorged_mort')
+  trans_matrix['fin', 'ein'] <- get_transition_fun('fn_en')
+  trans_matrix['fin', 'fin'] <- 1 - trans_matrix['fin', 'ein'] - get_transition_fun('en_m')
 
-  # TODO: implement as delay  
-  # trans_matrix['eun', 'qua'] <- get_transition_fun('nymph_engorged_adult', pred1 = temp)
-  # trans_matrix['eun', 'eun'] <- 1 - trans_matrix['eun', 'qua'] - get_transition_fun('adult_quest_mort')
-  # trans_matrix['ein', 'qia'] <- get_transition_fun('nymph_engorged_adult', pred1 = temp)
-  # trans_matrix['ein', 'ein'] <- 1 - trans_matrix['ein', 'qia'] - get_transition_fun('adult_quest_mort')
- 
   # trans_matrix['qua', 'fua'] skipped because depends on hosts
   # trans_matrix['qua', 'qua'] ...
   # trans_matrix['qia', 'fia'] ...
@@ -171,34 +139,86 @@ gen_trans_matrix <- function(time) {
 # 05 iteratively run model  
 
 run <- function(steps) {
-  # steps: number of iterations
+  # steps: number of iterations (days) to run model
   
   # update N[, time + 1]
   for (time in 1:(steps - 1)) {
 
     # generate transition matrix for (time -> time + 1), which is based on 
     # conditions (weather and host community) at day "time"
-    trans_matrix <- gen_trans_matrix(time, life_stages)
+    trans_matrix <- gen_trans_matrix(time)
     
     # update the delay matrix, also based on conditions at day "time"
     temp <- get_temp(time:(time + max_delay))
     
+    # TODO initial thoughts on how to generalize
+    # for (trans in c("e_hl", "eul_qun", "eil_qin", "eun_qua", "ein_qia", "ra_e")) { # development transitions only here
+    #   days <- cumsum(get_transition_fun(trans, pred1 = temp)) > 1
+    #   
+    #   if (TRUE %in% days) {
+    # 
+    #     # TODO the issue here is that we would need some way to identify the transition to grab based on the name of the transition
+    #     # options: (1) have a duplicate entry for each transition function and parameter each time it is used (e.g. duplicate for 
+    #     # eul_qun and eil_qin), or (2) use some string pattern matching, e.g. to get the transition fun/params "el_qn" for the 
+    #     # transitions "eul_qun" and "eil_qin"
+    #     
+    #     from <- str_split(trans, '_')[1]
+    #     to <- str_split(trans, '_')[2]
+    # 
+    #     days_to_next <- min(which(days))
+    #     surv_to_next <- 1 - get_transition_fun()
+    # 
+    #     # TODO need condition for multiplying by number of eggs
+    #     delay_mat[to, time + days_to_next] <- N[from, time]*surv_to_next + delay_mat[to, time + days_to_next] 
+    #   }
+    # }
+
     # for each transition using a delay, generate a vector of whether the cumsum is > 1 on each day
-    e_hl_days <- cumsum(get_transition_fun("egg_larva", pred1 = temp)) > 1
-    eul_qun_days <- cumsum(get_transition_fun('larva_engorged_nymph', pred1 = temp)) > 1
-    eil_qin_days <- cumsum(get_transition_fun('larva_engorged_nymph', pred1 = temp)) > 1    
+    e_hl_days <- cumsum(get_transition_fun("e_hl", pred1 = temp)) > 1
+    eul_qun_days <- cumsum(get_transition_fun('el_qn', pred1 = temp)) > 1
+    eil_qin_days <- cumsum(get_transition_fun('el_qn', pred1 = temp)) > 1    
+    eun_qua_days <- cumsum(get_transition_fun('en_qa', pred1 = temp)) > 1
+    ein_qia_days <- cumsum(get_transition_fun('en_qa', pred1 = temp)) > 1
+    ra_e_days <- cumsum(get_transition_fun('ra_e', pred1 = temp)) > 1
     
     # for each transition, if cumsum ever gets to 1
     if (TRUE %in% e_hl_days) {
-      days_to_hatch <- min(which(e_hl_days))
-      surv_to_hatch <- (1-get_transition_fun('egg_mort'))^days_to_hatch
-      delay_mat['hl',time+days_to_hatch] <- N['e',time]*surv_to_hatch + delay_mat['hl',time+days_to_hatch]
+      days_to_next <- min(which(e_hl_days))
+      surv_to_next <- (1-get_transition_fun('e_m'))^days_to_next
+      delay_mat['hl',time+days_to_next] <- N['e',time]*surv_to_next + delay_mat['hl',time+days_to_next]
     }
     
     if (TRUE %in% eul_qun_days) {
-      days_to_quest <- min(which(eul_qun_days))
-      surv_to_quest <- (1-get_transition_fun('larva_engorged_mort'))^days_to_quest
-      delay_mat['qun', time+days_to_quest] <- N['eul', time]*surv_to_hatch + delay_mat['qun', time+days_to_quest]
+      days_to_next <- min(which(eul_qun_days))
+      surv_to_next <- (1-get_transition_fun('el_m'))^days_to_next
+      delay_mat['qun', time+days_to_next] <- N['eul', time]*surv_to_next + delay_mat['qun', time+days_to_next]
+    }
+    
+    if (TRUE %in% eil_qin_days) {
+      days_to_next <- min(which(eul_qun_days))
+      surv_to_next <- (1-get_transition_fun('el_m'))^days_to_next
+      delay_mat['qin', time+days_to_next] <- N['eil', time]*surv_to_next + delay_mat['qin', time+days_to_next]
+    }
+    
+    if (TRUE %in% eun_qua_days) {
+      days_to_next <- min(which(eun_qua_days))
+      surv_to_next <- (1-get_transition_fun('en_m'))^days_to_next
+      delay_mat['qua', time+days_to_next] <- N['eun', time]*surv_to_next + delay_mat['qua', time+days_to_next]
+    }
+    
+    if (TRUE %in% ein_qia_days) {
+      days_to_next <- min(which(eil_qin_days))
+      surv_to_next <- (1-get_transition_fun('en_m'))^days_to_next
+      delay_mat['qia', time+days_to_next] <- N['ein', time]*surv_to_next + delay_mat['qia', time+days_to_next]
+    }
+    
+    # we hande egg laying similarly to other development transitions
+    # the difference is that we multiply the number of surviving reproductive adults by
+    # the number of eggs each adult produces
+    if (TRUE %in% ra_e_days) {
+      days_to_next <- min(which(ra_e_days))
+      surv_to_next <- (1-get_transition_fun('ra_m'))^days_to_next
+      delay_mat['e', time+days_to_next] <- N['ra', time]*surv_to_next * get_transition_fun('n_e') + delay_mat['e', time+days_to_next]
     }
     
     # now, we've used conditions at time "time" to predict future population sizes, so we can 
@@ -222,8 +242,8 @@ N_out <- run(dim(weather)[1])
 time <- 100
 temp2 <- v_temp(time:(time + max_delay))
 
-days_to_hatch <- min(which(cumsum(get_transition_fun("egg_larva", pred1 = temp2)) > 1))
-surv_to_hatch <- (1-get_transition_fun('egg_mort'))^days_to_hatch
+days_to_hatch <- min(which(cumsum(get_transition_fun("e_hl", pred1 = temp2)) > 1))
+surv_to_hatch <- (1-get_transition_fun('e_m'))^days_to_hatch
 
 delay_mat['hl',time+days_to_hatch] <- N['e',time]*surv_to_hatch + delay_mat['hl',time+days_to_hatch]
 
