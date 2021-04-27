f2b2d7908b0809da8054c17e606248c02be46c3c
Allen <dallen@middlebury.edu>
2021-04-27 10:35:38 -0400

    Using the new code structure and playing aroudn!

--- dave_script.R
+++ dave_script.R
@@ -16,7 +16,7 @@ source("testing_functions.R")
 
 # set constant steps, which ensures that model doesn't try to run for longer than there is input data
 # increased to see full cycle because with 15 degree temp, new eggs emerge around day 310
-steps <- 900
+steps <- 1200
 
 # At each time step, how many time steps should we look into the future to see if any
 # ticks emerge from a time delay? This value is a somewhat arbitrarily high constant.
@@ -24,23 +24,13 @@ max_delay <- 300
 
 # 00
 # read inputs 
-# weather <- read_csv('inputs/weather.csv')
-# constant temperature for testing
-# weather <- tibble(tmean = seq(from = 15, to = 15, length.out = steps), j_day = seq(from = 1, to = steps))
-# ogden weather 
-weather <- read_csv('inputs/2021-04-04_Myles_test/ogden_weather.csv')
-
-# non constant temp (sine wave approx Midd temp)
-# annualtemp <- 15*sin(2*pi/365*(x-110)) +5
-# repeatabunch <- rep(annualtemp,100)
-# weather <- tibble(tmean = repeatabunch[1:steps], j_day = seq(from = 1, to = steps))
+ weather <- tibble(tmean = seq(from = 15, to = 15, length.out = steps), j_day = seq(from = 1, to = steps))
 
 # host_comm <- read_csv('inputs/host_comm.csv') #### DA Note: will have to think about how to handle this
  host_comm <- tibble(
    j_day = rep(1:steps, each=3), 
    host_spp = rep(c('mouse', 'squirrel', 'deer'), steps), 
-   host_den = rep(c(100, 8, 8), steps)) %>%
-   # host_den = runif(steps * 3, .75, 1.25) * rep(c(40, 8, 0.25), steps)) %>% 
+   host_den = rep(c(1, 0.5, 0.1), steps)) %>%
    arrange(j_day, host_spp)
 
 
@@ -104,16 +94,19 @@ ggplot(out_N_df, aes(x = day, y = pop, color = process, shape = age_group, group
   scale_size_manual(values = c(1, 3)) + 
   scale_shape_manual(values = c(15:18)) + 
   geom_line() + 
-  scale_y_log10(limits = c(-1, 1e+05), breaks = c(10, 100, 1000, 10000, 100000, 1000000)) +
+  scale_y_log10(limits = c(1e+01, 1e+07), breaks = c(10, 100, 1000, 10000, 100000, 1000000)) +
   geom_hline(yintercept = 1000)
 
 # just see roughly whether population is increasing or decreasing
 out_N_df %>% 
-  filter(age_group == 'a') %>% 
+  #filter(age_group == 'a') %>% 
   group_by(day) %>% 
   summarise(tot = sum(pop)) %>% 
   mutate(lambda = tot/lag(tot)) %>% 
-  filter(is.finite(lambda) ) %>% 
-  ggplot(aes(day,lambda)) + 
-  geom_path()
+  filter(is.finite(lambda), lambda >0) %>%
+  summarise(lambda = exp(mean(log(lambda)))) %>%
+  as.numeric()
+
+
+
 
