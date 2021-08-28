1fda9d6b9e77b7fc6e863f6446264952918662bb
Myles Stokowski <mylesstokowski@gmail.com>
2021-08-28 11:43:46 -0400

    move scripts to own folder

--- R/scripts/IxPopDyMod.R
+++ R/scripts/IxPopDyMod.R
@@ -10,9 +10,9 @@ library(tidyr)
 library(ggplot2)
 library(igraph)
 
-source("core_functions.R")
-source("user_defined_functions.R")
-source("testing_functions.R")
+source("R/core_functions.R")
+source("R/user_defined_functions.R")
+source("R/testing_functions.R")
 
 # set constant steps, which ensures that model doesn't try to run for longer than there is input data
 # increased to see full cycle because with 15 degree temp, new eggs emerge around day 310
@@ -23,11 +23,11 @@ steps <- 3500
 max_delay <- 300
 
 # 00
-# read inputs 
+# read inputs
 # weather <- read_csv('inputs/weather.csv')
 # constant temperature for testing
 # weather <- tibble(tmean = seq(from = 15, to = 15, length.out = steps), j_day = seq(from = 1, to = steps))
-# ogden weather 
+# ogden weather
 weather <- read_csv('inputs/2021-04-04_Ogden/ogden_weather.csv')
 
 # non constant temp (sine wave approx Midd temp)
@@ -37,10 +37,10 @@ weather <- read_csv('inputs/2021-04-04_Ogden/ogden_weather.csv')
 
 # host_comm <- read_csv('inputs/host_comm.csv') #### DA Note: will have to think about how to handle this
 # host_comm <- tibble(
-#   j_day = rep(1:steps, each=3), 
-#   host_spp = rep(c('mouse', 'squirrel', 'deer'), steps), 
+#   j_day = rep(1:steps, each=3),
+#   host_spp = rep(c('mouse', 'squirrel', 'deer'), steps),
 #   host_den = rep(c(100, 8, 8), steps)) %>%
-#   # host_den = runif(steps * 3, .75, 1.25) * rep(c(40, 8, 0.25), steps)) %>% 
+#   # host_den = runif(steps * 3, .75, 1.25) * rep(c(40, 8, 0.25), steps)) %>%
 #   arrange(j_day, host_spp)
 
 # Ogden host community
@@ -61,7 +61,7 @@ if (simple) {
   tick_transitions <- read_csv('inputs/2021-04-04_Ogden/tick_transitions.csv')
   life_stages <- tick_transitions %>% pull(from) %>% unique()
 } else {
-  tick_params <- read_csv('inputs/tick_parameters.csv') %>% 
+  tick_params <- read_csv('inputs/tick_parameters.csv') %>%
     arrange(host_spp) # sort so parameters are in same host_spp order for pairwise vector calculations
   tick_transitions <- read_csv('inputs/tick_transitions.csv')
   life_stages <- tick_transitions %>% pull(from) %>% unique()
@@ -74,7 +74,7 @@ if (simple) {
   # initial_population['r_a'] <- 10 # start with only one cohort (adults)
   initial_population['q_a'] <- 10000 # Ogden initial population
 } else {
-  initial_population['rua'] <- 10 # start with only one cohort (adults)  
+  initial_population['rua'] <- 10 # start with only one cohort (adults)
 }
 
 # inspect test results before running
@@ -88,14 +88,14 @@ out_N <- out[[1]]
 out_N_developing <- out[[2]]
 out_delay_mat <- out[[3]]
 
-# get a more complete picture of the population size over time by summing the 
+# get a more complete picture of the population size over time by summing the
 # population matrix (of ticks not in delays) with the N_developing matrix (of ticks
 # currently undergoing a delay)
 out_N <- out_N + out_N_developing
 # As expected, out_N_developing only has nonzero entries corresponding to life stages
 # with delay transitions FROM that life stage, specifically:
 # c("__e" "eia" "eil" "ein" "eua" "eul" "eun" "f_l" "fia" "fin" "fua" "fun" "h_l")
-# These are all the feeding to engorged transitions, the engorged to next life stage transitions, 
+# These are all the feeding to engorged transitions, the engorged to next life stage transitions,
 # as well as egg to hardening larvae, and hardening to questing larvae
 # In turn, the model output (inspect graph or df) only has increased tick populations for these life stages
 
@@ -104,7 +104,7 @@ out_N <- out_N + out_N_developing
 # out_delay_mat[,1:50]
 
 # convert output population matrix to a friendly format for graphing
-out_N_df <- out_N %>% t() %>% as.data.frame() %>% mutate(day = row_number()) %>% 
+out_N_df <- out_N %>% t() %>% as.data.frame() %>% mutate(day = row_number()) %>%
   pivot_longer(-c(day), names_to = 'stage', values_to = 'pop') %>%
   mutate(age_group = age(stage),
          process = process(stage),
@@ -117,21 +117,21 @@ out_N_df <- out_N %>% t() %>% as.data.frame() %>% mutate(day = row_number()) %>%
 # (out_N_df$pop == prev_out_N_df$pop) %>% unique()
 
 # graph population over time
-ggplot(out_N_df, aes(x = day, y = pop, color = process, shape = age_group, group = stage)) + 
-  geom_point(aes(size = infected)) + # , position = 'jitter') + 
-  scale_size_manual(values = c(1, 3)) + 
-  scale_shape_manual(values = c(15:18)) + 
-  geom_line() + 
+ggplot(out_N_df, aes(x = day, y = pop, color = process, shape = age_group, group = stage)) +
+  geom_point(aes(size = infected)) + # , position = 'jitter') +
+  scale_size_manual(values = c(1, 3)) +
+  scale_shape_manual(values = c(15:18)) +
+  geom_line() +
   scale_y_log10(limits = c(-1, 1e+05), breaks = c(10, 100, 1000, 10000, 100000, 1000000)) +
   geom_hline(yintercept = 1000)
 
 # just see roughly whether population is increasing or decreasing
-out_N_df %>% 
-  filter(age_group == 'a') %>% 
-  group_by(day) %>% 
-  summarise(tot = sum(pop)) %>% 
-  mutate(lambda = tot/lag(tot)) %>% 
-  filter(is.finite(lambda) ) %>% 
-  ggplot(aes(day,lambda)) + 
+out_N_df %>%
+  filter(age_group == 'a') %>%
+  group_by(day) %>%
+  summarise(tot = sum(pop)) %>%
+  mutate(lambda = tot/lag(tot)) %>%
+  filter(is.finite(lambda) ) %>%
+  ggplot(aes(day,lambda)) +
   geom_path()
 
