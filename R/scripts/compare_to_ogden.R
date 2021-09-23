f47a43b027c0f00068e61a42be31fbb0c7d7eb62
Myles Stokowski <mylesstokowski@gmail.com>
2021-09-22 20:11:20 -0400

    better replicate Ogden fig4 - only use our model predictions for the days that Ogden sampled on, because Ogden only looks at model predictions for the days when they field sampled - minor figures updates

--- R/scripts/compare_to_ogden.R
+++ R/scripts/compare_to_ogden.R
@@ -6,65 +6,82 @@ library(gridExtra)
 source('inputs/2021-04-04_Ogden/make_ogden_fig4/make_ogden_fig4.R')
 out_N_df <- read_csv('outputs/ogden_output.csv')
 
-questing <- out_N_df %>% 
+questing <- out_N_df %>%
   # look at data after the model has run for a while and stabilized
-  mutate(post_eq_jday = day - 365 * 6) %>% 
+  mutate(post_eq_jday = day - 365 * 6) %>%
   filter(post_eq_jday > 0,
          post_eq_jday < 365 * 2,
          process == 'a')
 
-to_merge <- questing %>% 
-  group_by(age_group) %>% 
+ogden_sampling_days <- df %>%
+  pull(jday) %>%
+  unique()
+
+to_merge <- questing %>%
+  filter(post_eq_jday %in% ogden_sampling_days) %>%
+  group_by(age_group) %>%
   mutate(pop_fraction = pop/sum(pop)) %>%
   ungroup() %>%
   transmute(jday = post_eq_jday,
-            n = pop_fraction * 20, # multiply by constant just to scale so similar to Ogden
+            n = pop_fraction * 7, # multiply by constant just to scale so similar to Ogden
             group = str_c(process, age_group))
 
 merged <- rbind(df, to_merge)
 
-merged4a <- merged %>% 
-  filter(group %in% c('ol', 'pl', 'al')) %>% 
-  ggplot(aes(jday, n, color = group)) + 
-  geom_point() + 
-  geom_line()
+merged4a <- merged %>%
+  filter(group %in% c('pl', 'al')) %>%
+  ggplot(aes(jday, n, shape = group, linetype = group)) +
+  geom_point() +
+  geom_line() +
+  theme_classic() +
+  xlab("Day of year") +
+  ylab("Questing larvae")
 
-merged4b <- merged %>% 
-  filter(group %in% c('on', 'pn', 'an')) %>%
-  ggplot(aes(jday, n, color = group)) + 
-  geom_point() + 
-  geom_line()
+merged4b <- merged %>%
+  filter(group %in% c('pn', 'an')) %>%
+  ggplot(aes(jday, n, shape = group, linetype = group)) +
+  geom_point() +
+  geom_line() +
+  theme_classic() +
+  scale_color_viridis_d() +
+  xlab("Day of year") +
+  ylab("Questing nymphs")
 
 merged4c <- merged %>%
-  filter(group %in% c('oa', 'pa', 'aa')) %>% 
-  ggplot(aes(jday, n, color = group)) + 
-  geom_point() + 
-  geom_line()
+  filter(group %in% c('pa', 'aa')) %>%
+  ggplot(aes(jday, n, shape = group, linetype = group)) +
+  geom_point() +
+  geom_line() +
+  theme_classic() +
+  scale_color_viridis_d() +
+  xlab("Day of year") +
+  ylab("Questing adults")
+
 
 grid.arrange(merged4a, merged4b, merged4c)
 
 # OLD INTERPRETATION
-# Comparing our results to Ogden, increase in questing larvae is 
-# faster and decrease is slower, but I think that is because of how 
-# we define questing ticks -- as ticks that are ready to quest 
+# Comparing our results to Ogden, increase in questing larvae is
+# faster and decrease is slower, but I think that is because of how
+# we define questing ticks -- as ticks that are ready to quest
 # and may or may not be actively questing on any given day. I'm pretty
-# sure Ogden is modelling number of actively questing ticks on a 
-# given day, because that is what you can compare to the number of 
+# sure Ogden is modelling number of actively questing ticks on a
+# given day, because that is what you can compare to the number of
 # questing ticks collected in the field through drag sampling.
 
-# Perhaps the most obvious difference between our model and Ogden's is that 
-# the adult questing ticks peak earlier in our model. Instead, we should 
-# be comparing Ogden's results to the number of ATTACHED (not questing) ticks, 
-# because attached is the stage whose population is determined by the 
+# Perhaps the most obvious difference between our model and Ogden's is that
+# the adult questing ticks peak earlier in our model. Instead, we should
+# be comparing Ogden's results to the number of ATTACHED (not questing) ticks,
+# because attached is the stage whose population is determined by the
 # probability of questing (questing in our model just means
 # has emerged from development and will quest if the weather is right).
 
 # UPDATED INTERPRETATION
-# Having changed to looking at attached rather than questing ticks, 
+# Having changed to looking at attached rather than questing ticks,
 # we're getting a really close fit with Ogden's model. However, this is still
-# scaled by an arbitrary contant just to see phenology - the absolute numbers 
+# scaled by an arbitrary contant just to see phenology - the absolute numbers
 # don't mean anything at this point. One factor that would make this challenging
-# is that our attached ticks are dependent on the probability of finding hosts, 
+# is that our attached ticks are dependent on the probability of finding hosts,
 # which is a constant. This is different from Ogden's observed or modelled questing
-# ticks, which are presumably just based on probability of questing, not 
+# ticks, which are presumably just based on probability of questing, not
 # p(quest) * p(find host)
