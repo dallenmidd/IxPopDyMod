fd10cb23e15dd94ef1b55e7c0bea8a29429f3b44
Allen <dallen@middlebury.edu>
2021-11-11 15:24:49 -0500

    editting vignette

--- vignettes/host_example_config/make_inputs.R
+++ vignettes/host_example_config/make_inputs.R
@@ -3,13 +3,13 @@ steps <- 1200
 max_delay <- 365
 l <- steps + max_delay
 
-weather <- tibble(tmean = seq(from = 15, to = 15, length.out = l), j_day = seq(from = 1, to = l))
-write_csv(weather, 'inputs/2021-03-19_Dave_test/weather.csv')
+weather <- tibble(tmean = seq(from = 25, to = 25, length.out = l), j_day = seq(from = 1, to = l))
+write_csv(weather, 'vignettes/host_example_config/weather.csv')
 
 host_comm <- tibble(
   j_day = rep(1:l, each=3),
   host_spp = rep(c('mouse', 'squirrel', 'deer'), l),
-  host_den = rep(c(1, 0.5, 0.1), l)) %>%
+  host_den = rep(c(5, 2, 1), l)) %>%
   arrange(j_day, host_spp)
-write_csv(host_comm, 'inputs/2021-03-19_Dave_test/host_comm.csv')
+write_csv(host_comm, 'vignettes/host_example_config/host_comm.csv')
 
