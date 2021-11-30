446a5eb3e03bb23b4860def515310eec3b55374b
Allen <dallen@middlebury.edu>
2021-11-29 19:12:22 -0500

    My changes to the vignette

--- R/example_configs.R
+++ R/example_configs.R
@@ -28,7 +28,19 @@
 #'
 #' @format A \code{\link{config}}
 #'
-#' @seealso
-#' Based on Ogden et al. (2005) \doi{10.1016/j.ijpara.2004.12.013}
+#' #' \describe{
+#'   \item{steps}{Number of time steps to run the model. Here each step corresponds to one day.}
+#'   \item{initial_population}{Named vector of initial population size. Here the population starts with 10000 questing adults.}
+#'   \item{transitions}{A \code{\link{tibble}} giving the transitions between tick life stages.}
+#'   \item{parameters}{A \code{\link{tibble}} with the parameters to the life-stage transitions functions.}
+#'   \item{host_comm}{A \code{\link{tibble}} with the density of hosts over the model run. Here the host community is stable with 20 deer and 200 rodents.}
+#'   \item{weather}{A \code{\link{tibble}} with the average temperature for each day of the run.}
+#'   \item{max_delay}{The number of time units used for the delay functions.}
+#' }
 #'
+#' @seealso Ogden et al. (2005) \doi{10.1016/j.ijpara.2004.12.013}
+#' @examples
+#' data(ogden2005)
+#' output <- run(ogden2005)
+#' graph_population_each_group(output)
 "ogden2005"
