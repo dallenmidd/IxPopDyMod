c4e71742383b299f17abb426924cff65ba9dd277
Myles Stokowski <mylesstokowski@gmail.com>
2023-01-01 21:27:50 -0500

    start using checkmate for validation

--- R/parameters.R
+++ R/parameters.R
@@ -5,19 +5,18 @@ new_parameters <- function(...) {
 
   parameters <- structure(list(...), class="parameters")
 
-  stopifnot(all_elements_are_named(parameters))
-  stopifnot(!has_duplicate_names(parameters))
+  if (length(parameters) > 0) {
+    checkmate::assert_names(names(parameters), type = "unique")
+  }
 
   # Each parameter can be either a numeric vector of length 1, or a named
   # numeric vector of length > 1.
   # TODO need to figure out how to use vector names for matching parameters
   # to host species density predictor values
   for (param in parameters) {
-    stopifnot(is.numeric(param))
-    stopifnot(length(param) >= 1)
+    checkmate::assert_numeric(param, min.len = 1L)
     if (length(param) > 1) {
-      stopifnot(all_elements_are_named(param))
-      stopifnot(!has_duplicate_names(param))
+      checkmate::assert_names(names(param), type = "unique")
     }
   }
 
--- tests/testthat/test-parameters.R
+++ tests/testthat/test-parameters.R
@@ -6,11 +6,15 @@ test_that("new_parameters() works", {
 })
 
 test_that("new_parameters() throws eror when parameters are missing names", {
-  expect_error(new_parameters(1, 2))
-  expect_error(new_parameters(1, b = 2))
-  expect_error(new_parameters(a = 1, b = c(d = 2, 2)))
+  expect_error(new_parameters(1, 2), regexp = "Must have names")
+  expect_error(new_parameters(1, b = 2), "element 1 is empty")
+  expect_error(new_parameters(a = 1, b = c(d = 2, 2)), "element 2 is empty")
+})
+
+test_that("new_parameters() allows zero parameters", {
+  expect_equal(new_parameters(), structure(list(), class = "parameters"))
 })
 
 test_that("new_parameters() throws error if names are duplicated", {
-  expect_error(new_parameters(a = 1, a = 2))
+  expect_error(new_parameters(a = 1, a = 2), "element 2 is duplicated")
 })
