cfab4eab5380befba8396b407ec0cac9ff3ca472
Myles Stokowski <mylesstokowski@gmail.com>
2023-01-02 19:18:37 -0500

    remove restriction on number of function args

--- R/transition_function.R
+++ R/transition_function.R
@@ -3,14 +3,6 @@ new_transition_function <- function(raw_function) {
 
   checkmate::assert_function(raw_function)
 
-  if (length(formals(raw_function)) < 2) {
-    stop(
-      "transition functions must have at least 2 arguments, but ",
-      "only ", length(formals(raw_function)), " arg(s) were provided.",
-      call. = FALSE
-    )
-  }
-
   structure(
     raw_function,
     class = "transition_function"
