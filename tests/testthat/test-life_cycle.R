13dfa8790bad58eb7e45799a3d6311a043720d96
Myles Stokowski <mylesstokowski@gmail.com>
2023-01-07 22:59:05 -0500

    add tests

--- tests/testthat/test-life_cycle.R
+++ tests/testthat/test-life_cycle.R
@@ -7,3 +7,53 @@ test_that("output test", {
      )
   )
 })
+
+test_that("throws error with non-transition input", {
+  expect_error(
+    new_life_cycle(
+      transition_example_a(),
+      list()
+    ),
+    regexp = "May only contain the following types: {transition}",
+    fixed = TRUE
+  )
+})
+
+test_that("throws error with duplicate transitions", {
+  expect_error(
+    new_life_cycle(
+      transition_example_a(),
+      transition_example_a()
+    ),
+    "Contains duplicated values, position 2"
+  )
+})
+
+# life_cycle() ----------------------------------------------------------------
+test_that("handles transitions missing class attribute", {
+  expected <- life_cycle(
+    transition_example_a(),
+    transition_example_b()
+  )
+
+  # transitions should be coerced to `transition` type
+  result <- life_cycle(
+    unclass(transition_example_a()),
+    transition_example_b()
+  )
+
+  expect_identical(expected, result)
+})
+
+test_that("throws error with an invalid transition input", {
+  transition_missing_element <- transition_example_b()
+  transition_missing_element$parameters <- NULL
+  expect_error(
+    life_cycle(
+      transition_example_a(),
+      transition_missing_element
+    ),
+    regexp = "but is missing elements {'parameters'}",
+    fixed = TRUE
+  )
+})
