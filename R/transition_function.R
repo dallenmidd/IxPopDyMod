bf2fd3c10bc08a6bda075fcdc7e6b3eab9d73554
Myles Stokowski <mylesstokowski@gmail.com>
2023-01-19 22:16:59 -0500

    start updating config() to reflect modular structure

--- R/transition_function.R
+++ R/transition_function.R
@@ -1,6 +1,10 @@
 # TODO don't need this as a dedicated class anymore if the only check we're
 # doing is whether the input is a function
 #' Constructor for transition functions
+#'
+#' Transition functions must return a numeric vector. See
+#'   \code{\link{constant_fun}}, \code{\link{expo_fun}} and
+#'   \code{\link{infect_fun}} for examples for how to write custom functions.
 new_transition_function <- function(fun) {
 
   checkmate::assert_function(fun)
