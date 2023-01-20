03f5415182b9418bcfb564ea0618c94bca09ac35
Myles Stokowski <mylesstokowski@gmail.com>
2023-01-19 21:16:01 -0500

    fix typo

--- tests/testthat/test-parameters.R
+++ tests/testthat/test-parameters.R
@@ -5,7 +5,7 @@ test_that("new_parameters() works", {
   expect_equal(result, expected)
 })
 
-test_that("new_parameters() throws eror when parameters are missing names", {
+test_that("new_parameters() throws error when parameters are missing names", {
   expect_error(new_parameters(1, 2), regexp = "Must have names")
   expect_error(new_parameters(1, b = 2), "element 1 is empty")
   expect_error(new_parameters(a = 1, b = c(d = 2, 2)), "element 2 is empty")
