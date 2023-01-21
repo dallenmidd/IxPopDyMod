a91fb202e93b81c918c04a4ff7dfa57e1f551190
Myles Stokowski <mylesstokowski@gmail.com>
2023-01-21 17:36:03 -0500

    add templates for more tests

--- tests/testthat/test-config.R
+++ tests/testthat/test-config.R
@@ -1,3 +1,4 @@
+# output tests ----------------------------------------------------------------
 test_that("output test with NULL predictors", {
 
   # Need to set the environment so it doesn't change in snapshots
@@ -47,19 +48,31 @@ test_that("output test with non-NULL predictors", {
   )
 })
 
+# test simple checks on vector inputs -----------------------------------------
+test_that("works with integerish steps value", {})
 test_that("catches decimal steps value", {})
 test_that("catches negative steps value", {})
 test_that("catches missing steps value", {})
 test_that("catches steps of length > 1", {})
 
+test_that("works with integerish max_delay value", {})
 test_that("catches decimal max_delay value", {})
 test_that("catches max_delay value < 1", {})
 test_that("catches missing max_delay value", {})
 test_that("catches max_delay of length > 1", {})
 
+test_that("works with integerish initial_population values", {})
 test_that("catches decimal initial_population values", {})
 test_that("catches negative initial_population values", {})
 test_that("catches initial_population of length zero", {})
 test_that("catches duplicate life stage names in initial_population", {})
 
-test_that("initial_population set to zero for any unspecified life stages", {})
+# test more complicated checks / checks depending on multiple inputs ----------
+test_that("initial_population is set to zero for any unspecified life stages", {})
+test_that("catches initial_population with no values > 0", {})
+test_that("catches initial_population names that are not valid life statges", {})
+test_that("catches predictor data that does not extend to steps + max_delay", {})
+test_that("catches args in transition functions that don't correspond to a parameter or predictor", {})
+
+# TODO unsure if this is really feasible or should just be a runtime check
+test_that("catches transition functions that don't evaluate to a numeric", {})
