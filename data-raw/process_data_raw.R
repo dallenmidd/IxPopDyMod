d49b0e0a344ad03be059bdf8369c71dff3572446
Myles Stokowski <mylesstokowski@gmail.com>
2023-02-05 00:00:57 -0500

    update config_ex_2 to new structure

--- data-raw/process_data_raw.R
+++ data-raw/process_data_raw.R
@@ -21,8 +21,20 @@ config_ex_1 <- config(
 usethis::use_data(config_ex_1, overwrite = TRUE)
 
 # simple example config using delays
-config_ex_2 <- read_config("data-raw/config_ex_2/config_ex_2.yml")
-use_data(config_ex_2, overwrite = TRUE)
+config_ex_2 <- config(
+  cycle = life_cycle(
+    transition("__e", "__l", function(a) a, transition_type = "duration", parameters = c(a = 1)),
+    transition("__e", NULL, function(a) a, transition_type = "duration", mortality_type = "per_day", parameters = c(a = 0.01)),
+    transition("__l", "__n", function(a) a, transition_type = "duration", parameters = c(a = 0.01)),
+    transition("__l", NULL, function(a) a, transition_type = "duration", mortality_type = "per_day", parameters = c(a = 0.05)),
+    transition("__n", "__a", function(a) a, transition_type = "duration", parameters = c(a = 0.1)),
+    transition("__n", NULL, function(a) a, transition_type = "duration", mortality_type = "per_day", parameters = c(a = 0.1)),
+    transition("__a", "__e", function(a) a, transition_type = "probability", parameters = c(a = 489.3045))
+  ),
+  initial_population = c("__a" = 1000),
+  steps = 365L
+)
+usethis::use_data(config_ex_2, overwrite = TRUE)
 
 # recreate ogden et al. 2005 model in our package framework
 ogden2005 <- read_config("data-raw/ogden2005/ogden_config.yml")
