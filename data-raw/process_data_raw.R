d11fd90b5da6a5e62450a3b328a8cf277c2078d3
Myles Stokowski <mylesstokowski@gmail.com>
2023-02-04 23:20:28 -0500

    migrate config_ex_1 to new structure

--- data-raw/process_data_raw.R
+++ data-raw/process_data_raw.R
@@ -4,8 +4,21 @@ library(tidyverse)
 library(usethis)
 
 # super simple example config without delays
-config_ex_1 <- read_config("data-raw/config_ex_1/config_ex_1.yml")
-use_data(config_ex_1, overwrite = TRUE)
+config_ex_1 <- config(
+  cycle = life_cycle(
+    transition("__e", "__l", function(a) a, transition_type = "probability", parameters = c(a = 1)),
+    transition("__e", NULL, function(a) a, transition_type = "probability", mortality_type = "per_day", parameters = c(a = 0)),
+    transition("__l", "__n", function(a) a, transition_type = "probability", parameters = c(a = 0.01)),
+    transition("__l", NULL, function(a) a, transition_type = "probability", mortality_type = "per_day", parameters = c(a = 0.99)),
+    transition("__n", "__a", function(a) a, transition_type = "probability", parameters = c(a = 0.1)),
+    transition("__n", NULL, function(a) a, transition_type = "probability", mortality_type = "per_day", parameters = c(a = 0.9)),
+    transition("__a", "__e", function(a) a, transition_type = "probability", parameters = c(a = 1000)),
+    transition("__a", NULL, function(a) a, transition_type = "probability", mortality_type = "per_day", parameters = c(a = 0))
+  ),
+  initial_population = c("__a" = 1000),
+  steps = 29L
+)
+usethis::use_data(config_ex_1, overwrite = TRUE)
 
 # simple example config using delays
 config_ex_2 <- read_config("data-raw/config_ex_2/config_ex_2.yml")
