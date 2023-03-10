3c0f05baaa505250a21421b6c8116e8a666e0bc6
mylesstokowski <mylesstokowski@gmail.com>
2023-03-10 18:19:09 -0500

    update life_cycle formatting and include transitions

--- R/life_cycle.R
+++ R/life_cycle.R
@@ -148,11 +148,13 @@ coerce_transition <- function(index, transitions) {
 print.life_cycle <- function(x, ..., abbreviated = TRUE) {
   if (abbreviated) {
     cat(
-      "A life cycle",
-      "\n* Number of transitions:", length(x),
-      "\n* Life stages:", paste(life_stages(x), collapse = ", ")
+      "* A life cycle",
+      "\n** Number of transitions:", length(x),
+      "\n** Unique life stages:", paste(life_stages(x), collapse = ", "), "\n",
+      sapply(x, format.transition)
     )
   } else {
+    # print the default list representation
     print.default(x)
   }
 }
