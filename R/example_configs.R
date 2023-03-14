052c8f7d8441f37c6bc6af17d8e5fca08ca5795c
mylesstokowski <mylesstokowski@gmail.com>
2023-03-14 14:00:06 -0400

    update ogden2005 docs

--- R/example_configs.R
+++ R/example_configs.R
@@ -28,22 +28,12 @@
 #' this configuration to show that our package can be used to recreate existing
 #' models.
 #'
-#' @format A \code{\link{config}}
+#' In this config, the population starts with 10000 questing adults. The
+#' predictor data includes average temperature for each day, and density of
+#' hosts over the model run. Here the host community is stable with 20 deer and
+#' 200 mice.
 #'
-#' \describe{
-#'   \item{steps}{Number of time steps to run the model. Here each step
-#'   corresponds to one day.}
-#'   \item{initial_population}{Named vector of initial population size. Here
-#'   the population starts with 10000 questing adults.}
-#'   \item{transitions}{A \code{\link{tibble}} giving the transitions between
-#'   tick life stages.}
-#'   \item{parameters}{A \code{\link{tibble}} with the parameters to the
-#'   life-stage transitions functions.}
-#'   \item{predictors}{A \code{\link{tibble}} with the average temperature
-#'   for each day, and density of hosts over the model run. Here the host
-#'   community is stable with 20 deer and 200 rodents.}
-#'   \item{max_delay}{The number of time units used for the delay functions.}
-#' }
+#' @format A \code{\link{config}}
 #'
 #' @seealso Ogden et al. (2005) \doi{10.1016/j.ijpara.2004.12.013}
 #' @examples
