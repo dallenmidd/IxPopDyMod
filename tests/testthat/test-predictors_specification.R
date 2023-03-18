41996539e665709d18de70722e5e822e3540f09e
Myles Stokowski <mylesstokowski@gmail.com>
2023-03-18 13:42:24 -0400

    add a test

--- tests/testthat/test-predictors_specification.R
+++ tests/testthat/test-predictors_specification.R
@@ -6,3 +6,18 @@ test_that("output test", {
     )
   )
 })
+
+test_that("can coerce lists in predictors_spec to predictors_spec_node", {
+  # NOTE this is just a convenience feature - support could be removed and it
+  # would simplify the code. It could be useful though if we want to convert
+  # to/from YAML, as the class attribute may be dropped in process.
+  result <- predictors_spec(
+    a = list(name = "temp", first_day_only = FALSE),
+    b = list(name = "host_den", first_day_only = TRUE)
+  )
+  expected <- predictors_spec(
+    a = predictors_spec_node(name = "temp", first_day_only = FALSE),
+    b = predictors_spec_node(name = "host_den", first_day_only = TRUE)
+  )
+  expect_identical(expected, result)
+})
