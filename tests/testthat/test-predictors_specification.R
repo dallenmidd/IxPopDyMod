3ef95ce2f9b7b70ad5ebb1de4290f207bc55a3c5
Myles Stokowski <mylesstokowski@gmail.com>
2023-03-18 13:42:14 -0400

    update indentation

--- tests/testthat/test-predictors_specification.R
+++ tests/testthat/test-predictors_specification.R
@@ -1,14 +1,8 @@
 test_that("output test", {
   expect_snapshot(
     predictors_spec(
-      a = new_predictors_spec_node(
-        name = "temp",
-        first_day_only = FALSE
-      ),
-      b = new_predictors_spec_node(
-        name = "host_den",
-        first_day_only = TRUE
-      )
+      a = new_predictors_spec_node(name = "temp", first_day_only = FALSE),
+      b = new_predictors_spec_node(name = "host_den", first_day_only = TRUE)
     )
   )
 })
